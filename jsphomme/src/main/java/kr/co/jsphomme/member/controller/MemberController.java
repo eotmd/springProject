c13821de5246ba6bde6698d4d43966b657401ffe
hyemijung84 <hyemijung84@gmail.com>
2019-04-12 15:30:39 +0900

    작성자: hm 내용: webapp 아래 views 폴더 생성

---
+++ jsphomme/src/main/java/kr/co/jsphomme/member/controller/MemberController.java
@@ -0,0 +1,35 @@
+package kr.co.jsphomme.member.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.jsphomme.member.service.MemberService;
+
+
+@Controller
+public class MemberController {
+
+	private final Logger log = LoggerFactory.getLogger(MemberController.class);
+	
+	@Autowired
+	private MemberService memberService;
+	
+	// 회원 목록 조회 화면으로
+//	@RequestMapping
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
