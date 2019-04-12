ab5ec586705d15133a1e08a65bffa4c5a5897db1
wldnjsdk97 <wldnjsdk97@naver.com>
2019-04-12 16:45:44 +0900

    작성자: SJW
    상품 리스트 페이지 제작 중.
    
    상품 리스트 페이지 제작 완성해야 함.

--- jsphomme/src/main/java/kr/co/jsphomme/product/dao/ProductDaoImpl.java
+++ jsphomme/src/main/java/kr/co/jsphomme/product/dao/ProductDaoImpl.java
@@ -1,18 +1,37 @@
 package kr.co.jsphomme.product.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import kr.co.jsphomme.product.vo.ProductVo;
 
 public class ProductDaoImpl implements ProductDao{
 
+	@Autowired
+	SqlSessionTemplate sqlSession;
+	
+	String namespace = "kr.co.jsphomme";
+	
 	@Override
 	public ProductVo productInsert() {
 		// TODO Auto-generated method stub
+		
+		
+		
 		return null;
 	}
 
 	@Override
 	public ProductVo productListView() {
 		// TODO Auto-generated method stub
+		
+		Map<String, Object> map = new HashMap();
+		
+//		map.compute(key, remappingFunction)
+		
 		return null;
 	}
 
